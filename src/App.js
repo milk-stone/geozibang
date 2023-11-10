c49d989b1355a23583fcd6afc58e3eb830b0b070
milk-stone <ywshero@naver.com>
2023-11-10 21:42:53 +0900

    sss

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import React from "react";
+import Home from "./Home";
 
-import React from 'react';
-import Home from './Home';
-
-import { Link, Route, Switch, Routes, BrrowserRouter, BrowserRouter } from 'react-router-dom';
-
+import {
+  Link,
+  Route,
+  Switch,
+  Routes,
+  BrrowserRouter,
+  BrowserRouter,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path = '/' Component={Home} />
-        </Routes>  
+          <Route path="/" Component={Home} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
