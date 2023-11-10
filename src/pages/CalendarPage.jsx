ee308ffe25c822e1a55a297dd9deaa2939d8e563
toothlessdev <toothless042@gmail.com>
2023-11-11 00:04:26 +0900

    feat: mui calendar 구현

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,3 +1,26 @@
+import { useEffect, useState } from "react";
+import MainLayout from "../layouts/MainLayout";
+
+import dayjs, { Dayjs } from "dayjs";
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+
 export default function CalendarPage() {
-    return <main>calendarpage</main>;
+    const [value, setValue] = useState(dayjs("2022-04-17"));
+
+    useEffect(() => {
+        console.log(value.$y);
+        console.log(value.$M + 1);
+        console.log(value.$D);
+    }, [value]);
+
+    return (
+        <MainLayout>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} />
+            </LocalizationProvider>
+        </MainLayout>
+    );
 }
