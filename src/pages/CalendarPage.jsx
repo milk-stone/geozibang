fddbd21c5df2e16c63a3d5f1564024c76bc5505c
milk-stone <ywshero@naver.com>
2023-11-11 10:05:52 +0900

    달력 받아옴

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
+import { Title } from "../components/Title";
 import MainLayout from "../layouts/MainLayout";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
 
 import dayjs, { Dayjs } from "dayjs";
 import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
@@ -9,31 +10,23 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 
 export default function CalendarPage() {
-    const [value, setValue] = useState(dayjs("2022-04-17"));
+  const [value, setValue] = useState(dayjs("2022-04-17"));
 
-    useEffect(() => {
-        console.log(value.$y);
-        console.log(value.$M + 1);
-        console.log(value.$D);
-    }, [value]);
+  useEffect(() => {
+    console.log(value.$y);
+    console.log(value.$M + 1);
+    console.log(value.$D);
+  }, [value]);
 
-    return (
-        <MainLayout>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} />
-            </LocalizationProvider>
-        </MainLayout>
-    );
-=======
-import { Title } from "../components/Title";
-import MainLayout from "../layouts/MainLayout";
-import { faCalendar } from "@fortawesome/free-solid-svg-icons";
-
-export default function CalendarPage() {
   return (
     <MainLayout>
-      <Title icon={faCalendar}>달력 보기</Title>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Title icon={faCalendar}>달력 보기</Title>
+        <DateCalendar
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+        />
+      </LocalizationProvider>
     </MainLayout>
   );
->>>>>>> 10661df31fb96b2c7e582d4fa2a38ff051fe837c
 }
