fc3ce8a9a7939365576d7252bf89ecae9394221f
milk-stone <ywshero@naver.com>
2023-11-11 09:44:11 +0900

    임시

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,11 +1,32 @@
-import { Title } from "../components/Title";
+import { useEffect, useState } from "react";
 import MainLayout from "../layouts/MainLayout";
+import { Title } from "../components/Title";
 import { faCalendar } from "@fortawesome/free-solid-svg-icons";
 
+import dayjs, { Dayjs } from "dayjs";
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+
 export default function CalendarPage() {
+  const [value, setValue] = useState(dayjs("2022-04-17"));
+
+  useEffect(() => {
+    console.log(value.$y);
+    console.log(value.$M + 1);
+    console.log(value.$D);
+  }, [value]);
+
   return (
     <MainLayout>
-      <Title icon={faCalendar}>달력 보기</Title>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Title icon={faCalendar}>달력 보기</Title>
+        <DateCalendar
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+        />
+      </LocalizationProvider>
     </MainLayout>
   );
 }
