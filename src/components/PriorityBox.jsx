9213216ca92492793170abaeb51316437761275a
milk-stone <ywshero@naver.com>
2023-11-13 13:54:30 +0900

    submit 버튼 1회 사용 / 모달 창 닫을 시 GET 요청 전송

--- src/components/PriorityBox.jsx
+++ src/components/PriorityBox.jsx
@@ -10,35 +10,6 @@ export const PriorityBox = (props) => {
   const [state, setState] = useState(null);
   const [data, setData] = useState(null);
 
-  const handleSubmit = (formData) => {
-    const dateObj = new Date(formData.date);
-    formData.date = {
-      year: Number(dateObj.getFullYear()),
-      month: Number(dateObj.getMonth() + 1),
-      day: Number(dateObj.getDate()),
-    };
-    formData.checking = Number(formData.checking);
-    formData.ranking = Number(formData.ranking);
-    formData.money = Number(formData.money);
-
-    fetch("http://13.209.254.86:8080/sobi/create", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ formData }),
-    })
-      .then((response) => {
-        if (response.status !== 200) {
-          throw new Error("status != 200");
-        }
-        return response.json();
-      })
-      .then((data) => {})
-      .catch((error) => {});
-    setIsModalOpen(false);
-  };
-
   useEffect(() => {
     fetch("http://13.209.254.86:8080/sobi/readAll")
       .then((response) => {
@@ -62,15 +33,25 @@ export const PriorityBox = (props) => {
     }
   }, [state]);
 
-  const handleModalClose = () => {
-    setIsModalOpen(false);
-  };
-
   const handlePriorityButtonClick = () => {
     setIsModalOpen(true);
   };
 
   const handleCloseModal = () => {
+    fetch("http://13.209.254.86:8080/sobi/readAll")
+      .then((response) => {
+        if (response.status !== 200) throw new Error();
+        return response.json();
+      })
+      .then((data) => {
+        // 받아온 JSON 데이터를 상태에 저장
+        setState("success");
+        setData(data);
+      })
+      .catch((error) => {
+        setState("failed");
+        console.error("Error:", error);
+      });
     setIsModalOpen(false);
   };
   return (
@@ -84,7 +65,7 @@ export const PriorityBox = (props) => {
       {state === "success" &&
         data
           .filter((value) => value.checking !== 1)
-          .filter((value) => value.ranking == props.rank)
+          .filter((value) => value.ranking === props.rank)
           .map((element, index) => {
             return (
               <Contents
@@ -95,12 +76,7 @@ export const PriorityBox = (props) => {
             );
           })}
 
-      {isModalOpen && (
-        <ModalPopup
-          onClose={handleCloseModal}
-          onClick={handleSubmit}
-        ></ModalPopup>
-      )}
+      {isModalOpen && <ModalPopup onClose={handleCloseModal}></ModalPopup>}
     </div>
   );
 };
